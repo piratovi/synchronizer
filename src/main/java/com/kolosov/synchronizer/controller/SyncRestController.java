6e51645342ed4fe39aee687908b52d00b322c8d9
Andrey <piratovi@gmail.com>
2020-02-27 23:44:44 +0800

    .

--- src/main/java/com/kolosov/synchronizer/controller/SyncRestController.java
+++ src/main/java/com/kolosov/synchronizer/controller/SyncRestController.java
@@ -8,6 +8,7 @@ import com.kolosov.synchronizer.service.SyncService;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,12 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestController()
 @RequestMapping(value = "/rest")
 @Data
+@CrossOrigin
 public class SyncRestController {
 
     private final SyncService syncService;
